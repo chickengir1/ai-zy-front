494374f741a0c8777740ebee2efd1bf665d3e43e
Lee gangho <rkdgh1198@gmail.com>
2025-02-14 15:32:19 +0900

    feat : 프로젝트 카드 및 생성 아이템 컴포넌트 구현 #9 - ProjectCard 컴포넌트 구현 - CreationItem 컴포넌트 구현 - ParticipantIcons 컴포넌트 구현 - 스토리북 설정 추가 - 전역 스타일 정의

---
+++ src/components/chips/projects/ProjectCard.tsx
@@ -0,0 +1,52 @@
+import { twMerge } from "tailwind-merge";
+import { Link } from "react-router-dom";
+import { baseClasses, sizeClasses } from "@/utils/Styles/globalStyeld";
+import ParticipantIcons from "./ParticipantIcons";
+
+interface ProjectCardProps {
+  participantCount: number;
+  name: string;
+  description: string;
+  tag: string;
+  gradientFrom?: string;
+  gradientTo?: string;
+  linkTo?: string;
+}
+
+export default function ProjectCard({
+  participantCount,
+  name,
+  description,
+  tag,
+  gradientFrom,
+  gradientTo,
+  linkTo = "/",
+}: ProjectCardProps) {
+  return (
+    <Link to={linkTo}>
+      <div
+        className={twMerge(
+          "flex flex-col justify-end bg-gradient-to-b",
+          baseClasses.container,
+          sizeClasses.container,
+          gradientFrom,
+          gradientTo
+        )}
+      >
+        <span className="m-2 w-fit rounded bg-black px-2 py-1 text-sm text-white">
+          {tag}
+        </span>
+        <div className="flex flex-col gap-1 rounded-b-2xl border bg-[#f9f9f9] p-4">
+          <h2 className="text-lg font-bold text-gray-700">{name}</h2>
+          <p className="text-sm text-gray-500">{description}</p>
+          <div className="flex items-center justify-between">
+            <ParticipantIcons count={participantCount} />
+            <span className="text-sm text-gray-800">
+              {participantCount}명의 참가자
+            </span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
---
+++ src/utils/Styles/globalStyeld.ts
@@ -0,0 +1,17 @@
+export const baseClasses = {
+  container:
+    "cursor-pointer rounded-2xl shadow-lg transition-shadow hover:shadow-xl",
+  linkButton: "flex h-full w-full flex-col items-center justify-center",
+  iconContainer:
+    "flex items-center justify-center rounded-full border-2 border-dashed border-gray-300",
+  icon: "text-gray-400",
+  text: "font-medium text-gray-500",
+};
+
+export const sizeClasses = {
+  container: "h-[250px] w-full max-w-[350px] md:h-[300px] ",
+  linkButton: "gap-4 md:gap-3",
+  iconContainer: "h-12 w-12 md:h-10 md:w-10",
+  icon: "h-6 w-6 md:h-5 md:w-5",
+  text: "text-base md:text-lg",
+};
